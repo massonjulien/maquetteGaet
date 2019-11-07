5f3c3ff62a66a7b6125ef179dab94e194a199dbc
massonjulien <32932842+massonjulien@users.noreply.github.com>
2019-11-07 15:36:18 +0100

    Add files via upload

---
+++ gt/html5up-phantom/assets/js/db.js
@@ -0,0 +1,6 @@
+var occasions = [
+  [1, "Citroën Zoé","occasions1.jpg","2012","80.000km", "6.500€"],
+  [2, "Citroën C1","occasions3.jpg","2017","10.000km", "9.800€"],
+  [3, "Citroën C3","occasions2.jpg","2007","170.000km", "2.600€"],
+  [4, "Honda Civi","occasions4.jpg","2005","230.000km", "1.800€"],
+]
